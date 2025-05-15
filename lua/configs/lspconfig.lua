66c5f1223545a45f1c41b47079881a8bf32aa232
Dr SpongeBob <23226080@life.hkbu.edu.hk>
2025-05-15 15:26:57 +0800

    remove all lsp servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,19 +1,6 @@
 require('nvchad.configs.lspconfig').defaults()
 
-local servers = {
-  'bashls',
-  'clangd',
-  'cssls',
-  'html',
-  'java_language_server',
-  'jsonls',
-  'luau_lsp',
-  'marksman',
-  'pyright',
-  'rust_analyzer',
-  'ts_ls',
-  'vimls',
-}
+local servers = {}
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
